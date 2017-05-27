4231824682e91e9bfcd0b2f769693691e48993cd
VladKRP <karpovichvladislavitsp@gmail.com>
2017-05-28 01:02:20 +0300

    Bot works with phrase 'Как доехать из место1 в место2'

--- BestTickets/RouteHelpBot/Controllers/MessagesController.cs
+++ BestTickets/RouteHelpBot/Controllers/MessagesController.cs
@@ -10,6 +10,8 @@ using Newtonsoft.Json;
 using BestTickets.Models;
 using BestTickets.Controllers;
 using System.Text;
+using BestTickets.Services;
+using System.Collections.Generic;
 
 namespace RouteHelpBot
 {
@@ -25,39 +27,63 @@ namespace RouteHelpBot
             if (activity.Type == ActivityTypes.Message)
             {
                 ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                var request = activity.Text;
-                var departurePlace = request.Where((x,i) => i > request.IndexOf("из") + 2 && i < request.IndexOf("в") - 1);
-                var arrivalPlace = request.Where((x,i) => i > request.IndexOf("в"));
-                departurePlace = string.Join("", departurePlace);
-                arrivalPlace = string.Join("", arrivalPlace);
-                RouteViewModel route = new RouteViewModel(departurePlace.ToString(), arrivalPlace.ToString(), null);
 
-                var controller = new TicketsController();
+                var feedback = HandleUserRequest(activity);
 
-                var tickets = controller.GetTickets(route);
-
-
-                var replyMessage = new StringBuilder();
-                if (tickets.Count() > 0)
-                {
-                    replyMessage.Append($"Билеты по маршруту {arrivalPlace} - {departurePlace} :\n ");
-                    foreach (var ticket in tickets)
-                    {
-                        replyMessage.Append($"---\nНазвание транспорта:{ticket.Name}\nОтправление:{ticket.DepartureTime}\nПрибытие:{ticket.ArrivalTime}\nМеста:{ticket.Places}\n---\n");
-                    }
-                }
-                else
-                    replyMessage.Append($"Билетов по вашему запросу не найденно.Возможно вы указали что-то не верно.");
-
-                Activity reply = activity.CreateReply(replyMessage.ToString());
+                Activity reply = activity.CreateReply(feedback);
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
             {
-                HandleSystemMessage(activity);
+                 HandleSystemMessage(activity);
             }
             var response = Request.CreateResponse(HttpStatusCode.OK);
             return response;
+        }
+
+        private string HandleUserRequest(Activity activity)
+        {
+            var request = activity.Text;
+            var departurePlace = request.Where((x, i) => i > request.IndexOf("из") + 2 && i < request.IndexOf("в") - 2);
+            var arrivalPlace = request.Where((x, i) => i > request.IndexOf("в") + 1);
+            departurePlace = string.Join("", departurePlace);
+            arrivalPlace = string.Join("", arrivalPlace);
+            RouteViewModel route = new RouteViewModel(departurePlace.ToString(), arrivalPlace.ToString(), null);
+            var tickets = new TicketsController().GetTickets(route);
+            return GenerateFeedbackMessage(tickets); 
+        }
+
+        private string GenerateFeedbackMessage(IEnumerable<Vehicle> tickets)
+        {
+            var feedbackMessage = new StringBuilder();
+            if (tickets.Count() > 0)
+            {
+                feedbackMessage.Append($"Вот что я нашел по вашему запросу:\n ");
+                foreach (var ticket in tickets)
+                {
+
+                    feedbackMessage.Append($"---\n\r{ticket.Name} {ticket.Type}\n\rОтправление:{ticket.DepartureTime}\n\rПрибытие:{ticket.ArrivalTime}\n\r");
+                    if (ticket.Places.Count() > 0)
+                    {
+                        feedbackMessage.Append($"Места:\n\r");
+                        foreach (var place in ticket.Places)
+                            feedbackMessage.Append($"{place.Type}/{place.Amount}/{place.Cost} руб.\n\r");
+                    }
+
+                }
+            }
+            else
+                feedbackMessage.Append(MakeNotFoundFeedbackUntrivial());
+            return feedbackMessage.ToString();
+        }
+
+        private string MakeNotFoundFeedbackUntrivial()
+        {
+            var feedback = new List<string>() { "Билетов по вашему запросу не найденно.Возможно вы указали что-то не верно.",
+                "Мне не удалось ничего найти. Возможно что-то указано не верно.",
+                "Возможно вы в чем-то ошиблись. Мне не удалось ничего найти.",
+           "По вашему запросу ничего не найдено. Возможно вы что-то ввели неверно." };
+            return feedback.ElementAt(new Random().Next(feedback.Count()));
         }
 
         private Activity HandleSystemMessage(Activity message)
