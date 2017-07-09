9d0d3d57600f9c71a5627ecfe1b8aaa79e72901b
VladKRP <karpovichvladislavitsp@gmail.com>
2017-07-09 03:26:30 +0300

    Create new project and move changed model to it

---
+++ BestTickets/BestTicket.Domain/Abstractions/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BestTickets.Domain.Abstractions
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        T Get(int id);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(int id);
+        void Save();
+    }
+}
---
+++ BestTickets/BestTicket.Domain/Abstractions/IRouteRequestRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using BestTickets.Domain.Models;
+
+namespace BestTickets.Domain.Abstractions
+{
+    public interface IRouteRequestRepository:IRepository<RouteRequest>
+    {
+        IQueryable<RouteRequest> GetTop10();
+        RouteRequest GetByRoute(Route route);
+    }
+}
