1900517d9a8ffe3e18f5d5a266d3fdbe5895455d
VladKRP <karpovichvladislavitsp@gmail.com>
2017-07-09 03:23:17 +0300

    Delete unnecessary logic from model classes

---
+++ BestTickets/BestTicket.Domain/Models/RouteRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BestTickets.Domain.Models
+{
+    public class RouteRequest
+    {
+        public int Id { get; set; }
+        public Route Route { get; set; }
+        public int RequestsCount { get; set; } = 0;
+    }
+}
