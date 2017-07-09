53cea0973d2a77e515be2e725b369f36d1191a9b
VladKRP <karpovichvladislavitsp@gmail.com>
2017-07-09 03:37:26 +0300

    Move abstractions to the domain model. Update class that implement interface

--- BestTickets/BestTickets/Infrastructure/RouteRequestContext.cs
+++ BestTickets/BestTickets/Infrastructure/RouteRequestContext.cs
@@ -1,6 +1,6 @@
 ﻿using System.Data.Entity;
-using BestTickets.Models;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using BestTickets.Domain.Models;
 
 namespace BestTickets.Infrastructure
 {
